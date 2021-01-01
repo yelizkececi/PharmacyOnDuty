c953bbf4b5b836d08ac1c5ded26461c6807d005a
Yeliz Kececi <yelizkececi>
2021-01-01 21:41:24 +0300

    Initial Commit

---
+++ PharmacyOnDuty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PharmacyOnDuty
+//
+//  Created by Yeliz Keçeci on 1.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
